b891d050ba587718625bf4b66c7e9ec9568f41df
AbdalrhmanFiras <aa3819341@gmail.com>
2025-03-21 22:27:15 +0300

    implement messages_table

--- database/migrations/2025_03_21_192032_create_messages_table.php
+++ database/migrations/2025_03_21_192032_create_messages_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,11 @@ return new class extends Migration
     {
         Schema::create('messages', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('sender_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('receiver_id')->constrained('users')->onDelete('cascade');
+            $table->text('message')->nullable();
+            $table->string('file_path')->nullable();// for images
+            $table->timestamp('read_at');//for the seen
             $table->timestamps();
         });
     }
