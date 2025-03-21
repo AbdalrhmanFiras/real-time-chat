0664ca2f2989cbeb1a43ef117800595175cf17b0
AbdalrhmanFiras <aa3819341@gmail.com>
2025-03-21 22:51:51 +0300

    Add relation in message model

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Message extends Model
 {
 
+    public function Sender()
+    {
+        return $this->belongsTo(User::class, 'sender_id');
+    }
+
+    public function Receiver()
+    {
+        return $this->belongsTo(User::class, 'receiver_id');
+    }
+
 }
