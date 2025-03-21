89c4325252669d84e8cca0ca0d9c0c437fd906b0
AbdalrhmanFiras <aa3819341@gmail.com>
2025-03-21 23:55:19 +0300

    Add login function & route & hasApiToken

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 use App\Models\User;
-use Hash;
 use App\Http\Requests\RegisterForm;
 use Illuminate\Http\Request;
+use Hash;
+use Auth;
 class AuthController extends Controller
 {
     public function register(RegisterForm $request)
@@ -21,6 +22,23 @@ class AuthController extends Controller
         return response()->json([
             'user' => $user,
             'message' => 'user registerd successfully'
+        ], 200);
+
+    }
+
+
+    public function login(Request $request)
+    {
+
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json(['message' => 'invalid email or password'], 401);
+        }
+
+        $user = User::where('email', $request->email)->firstOrFail();//get the user by email
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token = $user->createToken('auth-token')->plainTextToken
         ], 200);
 
     }
